00c2129694da79186eefd958c54b49512212191b
athanase-galatsidas <athanase.galatsidas@gmail.com>
2021-05-02 23:30:06 +0200

    updated startup.cs

--- Startup.cs
+++ Startup.cs
@@ -94,9 +94,9 @@ namespace backendproject
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "backendproject v1"));
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "backendproject v1"));
 
             app.UseHttpsRedirection();
 
